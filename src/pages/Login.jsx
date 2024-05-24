0e2397f327a9645698756339acee647701841db4
Luis Morales <lfmoralesc@gbm.com.mx>
2024-05-24 04:23:42 -0600

    modified login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,9 @@ import { Row, Col } from "antd";
 import { Form, Input, Button } from "antd";
 import { useNavigate } from "react-router-dom";
 import CustomCarousel from "@components/General/CustomCarousel";
-import Logo from "@assets/images/Logo.png";
 import AnimationLayout from "@components/General/AnimationLayout";
 import { useLogin } from "@hooks/Users/useLogin";
+import Logo from "@assets/images/Logo.png";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -17,83 +17,81 @@ export default function Login() {
   const { login } = useLogin();
 
   const onFinish = (values) => {
-    login(values)
+    login(values);
   };
 
   return (
     // <AnimationLayout>
-      <div className="flex h-screen ">
-        <Row className="w-full h-full">
-          <Col
-            xs={{ span: 24 }}
-            sm={{ span: 24 }}
-            md={{ span: 12 }}
-            lg={{ span: 12 }}
-            xl={{ span: 12 }}
-            className="flex items-center justify-center p-4 md:p-16"
+    <div className="flex h-screen ">
+      <Row className="w-full h-full">
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 12 }}
+          lg={{ span: 12 }}
+          xl={{ span: 12 }}
+          className="flex items-center justify-center p-4 md:p-16"
+        >
+          <Form
+            name="login-form"
+            onFinish={onFinish}
+            layout="vertical"
+            initialValues={{ remember: true }}
+            style={{ width: "100%", margin: "0 auto" }}
           >
-            <Form
-              name="login-form"
-              onFinish={onFinish}
-              layout="vertical"
-              initialValues={{ remember: true }}
-              style={{ width: "100%", margin: "0 auto" }}
+            <div className="flex items-center justify-center">
+              <img src={Logo} className="mb-16 h-28"></img>
+            </div>
+            <Form.Item
+              label="Correo electrónico"
+              className="text-lg font-bold"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingresa tu correo electrónico.",
+                },
+              ]}
             >
-              <Form.Item
-                label="Correo electrónico"
-                name="email"
-                rules={[
-                  {
-                    required: true,
-                    message: "Por favor ingresa tu correo electrónico.",
-                  },
-                ]}
-              >
-                <Input />
-              </Form.Item>
+              <Input />
+            </Form.Item>
 
-              <Form.Item
-                label="Contraseña"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Por favor ingresa tu contraseña.",
-                  },
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
+            <Form.Item
+              label="Contraseña"
+              name="password"
+              className="text-lg font-bold"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingresa tu contraseña.",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-              <Form.Item>
-                <Button
-                  style={{ backgroundColor: "#008FD1" }}
-                  type="primary"
-                  htmlType="submit"
-                  className="w-full my-4"
-                >
-                  Iniciar sesión
-                </Button>
-                {/* <Button
-                  onClick={() => navigate("/Register")}
-                  className="w-full"
-                >
-                  Registrarse
-                </Button> */}
-              </Form.Item>
-            </Form>
-          </Col>
-          <Col
-            xs={{ span: 0 }}
-            sm={{ span: 0 }}
-            md={{ span: 12 }}
-            lg={{ span: 12 }}
-            xl={{ span: 12 }}
-          >
-            <CustomCarousel />
-          </Col>
-        </Row>
-      </div>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="w-full my-4 h-auto rounded-full bg-orange-500"
+              >
+                <p className="text-center py-2 align-middle justify-center text-lg font-bold">Iniciar sesión</p>
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+        <Col
+          xs={{ span: 0 }}
+          sm={{ span: 0 }}
+          md={{ span: 12 }}
+          lg={{ span: 12 }}
+          xl={{ span: 12 }}
+        >
+          <CustomCarousel />
+        </Col>
+      </Row>
+    </div>
     // </AnimationLayout>
   );
 }
