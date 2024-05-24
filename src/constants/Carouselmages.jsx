88653297409cf2491b19de9038d76a53d0046ed9
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-05-24 03:33:02 -0600

    diferent assets

--- src/constants/Carouselmages.jsx
+++ src/constants/Carouselmages.jsx
@@ -1,21 +1,22 @@
-import B1 from "@assets/images/B1.jpg";
-import B2 from "@assets/images/B2.jpg";
-import B3 from "@assets/images/B3.jpg";
+import B3 from "@assets/images/B1.jpeg";
+import B2 from "@assets/images/B2.jpeg";
+import B1 from "@assets/images/B3.jpeg";
 
 export const CarouselImages = [
-    {
-      image: B1,
-      area: "DISEÑO WEB",
-      text: "Sitios que lucen bien en cualquier tamaño",
-    },
-    {
-      image: B2,
-      area: "PANORAMAS",
-      text: "Recorridos virtuales",
-    },
-    {
-      image: B3,
-      area: "SISTEMAS",
-      text: "Desarrollo de software",
-    },
-  ];
+  {
+    area: "Calles",
+    text: "Reportar problemas como baches o señalización defectuosa en las calles.",
+    image: B1,
+  },
+  {
+    area: "Tuberías",
+    text: "Informar fugas de agua, problemas de drenaje o roturas en las tuberías.",
+    image: B2,
+
+  },
+  {
+    area: "Mobiliario urbano",
+    text: "Reportar daños en bancos, papeleras u otros elementos del mobiliario urbano.",
+    image: B3,
+  },
+];
